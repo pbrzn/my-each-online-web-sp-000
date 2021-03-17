d603dea82927328e69914a1bc2d152d58cd9d1c9
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-17 21:26:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i=0
+    while i<array.length
+      yield(array[i])
+      i+=1
+    end
+    array
+  else
+    array
+  end
 end
+
+my_each([1,2,3,4]){|i| puts i}
